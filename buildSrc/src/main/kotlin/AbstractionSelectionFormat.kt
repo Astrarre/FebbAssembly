600c20dd85c4a9df28412d0d42421a95ee09c994
natanfudge <natan.lifsiz@gmail.com>
2020-07-16 11:24:47 +0300

    doc

--- buildSrc/src/main/kotlin/AbstractionSelectionFormat.kt
+++ buildSrc/src/main/kotlin/AbstractionSelectionFormat.kt
@@ -2,6 +2,19 @@ import com.typesafe.config.ConfigFactory
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.config.ConfigParser
 
+/**
+ * package/name {
+ *     ClassName {
+ *         regex = true
+ *         methods  = ["^method1()V$", "method2"]
+ *         fields = [...]
+ *     }
+ * }
+ *
+ * if methods is null then all are accepted; if fields is null then all are accepted
+ * regex defaults to false and affects the class name, method names and field names
+ */
+
 @Serializable
 data class AbstractionSelection(val value: Map<String, AbstractedPackage>) {
     companion object {
